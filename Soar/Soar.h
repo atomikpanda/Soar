4c5162142a0ea1c9957cfed5e543eb73af1e989e
Bailey Seymour <baileyseymour@Baileys-MacBook-Pro.local>
2014-09-24 09:32:05 -0400

    Initial Commit

---
+++ Soar/Soar.h
@@ -0,0 +1,19 @@
+//
+//  Soar.h
+//  Soar
+//
+//  Created by Bailey Seymour on 9/24/14.
+//  Copyright (c) 2014 Bailey Seymour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Soar.
+FOUNDATION_EXPORT double SoarVersionNumber;
+
+//! Project version string for Soar.
+FOUNDATION_EXPORT const unsigned char SoarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Soar/PublicHeader.h>
+
+
